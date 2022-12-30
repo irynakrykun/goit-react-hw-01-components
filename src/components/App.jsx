c29eb9de7187b64f029cc52ac895f2a628f43fef
irynakrykun <ira_franyk@ukr.net>
2022-12-30 18:49:12 +0200

    chek

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-// import user from './user.json';
+import user from '../user.json';
 import { Profile } from './Profile/Profile';
 export const App = () => {
   return (
     <div>
       <Profile
-  // username={user.username}
-  // tag={user.tag}
-  // location={user.location}
-  // avatar={user.avatar}
-  // stats={user.stats}
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
 />
     </div>
           
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,29 @@
-import PropTypes from 'prop-types';
-export const Profile = ({username,tag,location,avatar,stats}) => {
+import propTypes from 'prop-types';
+export const Profile = ({username, tag, location, avatar, stats}) => {
     return (
         <div class="profile">
             <div class="description">
                 <img
-                    src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+                    src={avatar}
                     alt="User avatar"
                     class="avatar"
                 />
-                <p class="name">Petra Marica</p>
-                <p class="tag">@pmarica</p>
-                <p class="location">Salvador, Brasil</p>
+                <p class="name">{username}</p>
+                <p class="tag">@{tag}</p>
+                <p class="location">{location }</p>
             </div>
 
             <ul class="stats">
                 <li>
-                    <span class="label">Followers</span>
+                    <span class="label">{stats.followers }</span>
                     <span class="quantity">1000</span>
                 </li>
                 <li>
-                    <span class="label">Views</span>
+                    <span class="label">{stats.views }</span>
                     <span class="quantity">2000</span>
                 </li>
                 <li>
-                    <span class="label">Likes</span>
+                    <span class="label">{stats.likes }</span>
                     <span class="quantity">3000</span>
                 </li>
             </ul>
@@ -32,14 +32,26 @@ export const Profile = ({username,tag,location,avatar,stats}) => {
 };
 
 
-Profile.PropTypes = {
-    username: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    stats: PropTypes.shape({
-        followers: PropTypes.number.isRequired,
-        views: PropTypes.number.isRequired,
-        likes: PropTypes.number.isRequired,
+Profile.propTypes = {
+    username: propTypes.string.isRequired,
+    tag: propTypes.string.isRequired,
+    location: propTypes.string.isRequired,
+    avatar: propTypes.string.isRequired,
+    stats: propTypes.shape({
+        followers: propTypes.number.isRequired,
+        views: propTypes.number.isRequired,
+        likes: propTypes.number.isRequired,
     })
 };
+
+//  const user = {
+//     "username": "Jacques Gluke",
+//     "tag": "jgluke",
+//     "location": "Ocho Rios, Jamaica",
+//     "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//     "stats": {
+//         "followers": 5603,
+//         "views": 4827,
+//         "likes": 1308
+//     }
+// }
