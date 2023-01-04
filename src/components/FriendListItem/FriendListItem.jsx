8037c7b28c8e06febee4cb3217e6ed704b6cffd8
irynakrykun <ira_franyk@ukr.net>
2023-01-04 20:05:03 +0200

    chtck

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,11 +3,12 @@ import css from '../FriendListItem/FriendListItem.module.css';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      {isOnline ? (
-        <span className={css.statusOnline}></span>
-      ) : (
-        <span className={css.statusOffline}></span>
-      )}
+      <span  className={ 
+        (isOnline)
+        ?
+          "{css.statusOnline}"
+          :"{css.statusOffline}"}
+      ></span>
 
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
